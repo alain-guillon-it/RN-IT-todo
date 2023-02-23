bce8410f79670a6c9656b28bcc6d594984ff0911
Alain Guillon <a.guillon@it-students.fr>
2023-02-23 15:16:09 +0100

    step-3

--- App.js
+++ App.js
@@ -6,15 +6,16 @@ import {
   Text,
   View,
   Button,
-  Separator,
   FlatList,
+  ScrollView,
 } from "react-native";
 import { useState } from "react";
 
 export default function App() {
-  // STEP-2
+  // STEP-2 & 3
 
-  const [todo, onChangeTodo] = useState([
+  // Liste des tâches
+  const [todo, setTodo] = useState([
     "Faire les courses",
     "Aller à la salle de sport 3 fois par semaine",
     "Monter à plus de 5000m d altitude",
@@ -25,76 +26,103 @@ export default function App() {
     "Faire une mission en freelance",
     "Organiser un meetup autour de la tech",
     "Faire un triathlon",
+    "Apprendre React Native",
+    "Apprendre NodeJS",
+    "Apprendre React",
+    "Apprendre NextJS",
+    "Apprendre Symfony",
   ]);
+
+  // Edition de l'input texte
+  const [newTodo, setNewTodo] = useState("");
+
+  // A
+  function handleClickTodo() {
+    if (newTodo.trim() === "") {
+      return;
+    }
+    setTodo([...todo, newTodo]);
+    setNewTodo("");
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodos = [...todo];
+    newTodos.splice(index, 1);
+    setTodo(newTodos);
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      {/* STEP - 1 */}
+      <ScrollView style={styles.scrollView}>
+        {/* STEP - 2 ET 3 */}
 
-      {/* <Text
-        style={{
-          fontWeight: "900",
-        }}
-      >
-        Je suis un premier message
-      </Text>
-
-      <Text
-        style={{
-          color: "crimson",
-        }}
-      >
-        Et moi un second message
-      </Text> */}
-
-      {/* STEP - 2 */}
-
-      <Text
-        style={{
-          fontSize: 32,
-          textAlign: "center",
-          marginVertical: 16,
-        }}
-      >
-        My Todo List
-      </Text>
-
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <TextInput
-          onChangeText={onChangeTodo}
-          value={todo}
-          style={styles.input}
-          placeholder="Add a new todo"
-        ></TextInput>
-
-        <Button
-          title="Add"
+        <Text
           style={{
-            width: 25,
+            fontSize: 32,
+            textAlign: "center",
+            marginVertical: 16,
           }}
-          // onClick={handleClickTodo}
-        ></Button>
-      </View>
+        >
+          My Todo List
+        </Text>
 
-      <View
-        style={{
-          marginVertical: 16,
-        }}
-      >
-        {todo && (
-          <FlatList
-            data={todo}
-            renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
-          ></FlatList>
-        )}
-      </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <TextInput
+            onChangeText={(text) => setNewTodo(text)}
+            value={newTodo}
+            style={styles.input}
+            placeholder="Add a new todo"
+          ></TextInput>
 
-      <StatusBar style="auto" />
+          <Button
+            title="Add"
+            style={{
+              width: "20%",
+            }}
+            onPress={handleClickTodo}
+          ></Button>
+        </View>
+
+        <View
+          style={{
+            marginVertical: 16,
+          }}
+        >
+          {todo && (
+            <FlatList
+              data={todo}
+              renderItem={({ item, index }) => (
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginVertical: 5,
+                    padding: 8,
+                    borderWidth: 2,
+                    border: 1,
+                  }}
+                >
+                  <Text style={styles.item}>{item}</Text>
+                  <Button
+                    title="X"
+                    color="crimson"
+                    style={{ elevation: 5 }}
+                    onPress={() => handleDeleteTodo(index)}
+                  ></Button>
+                </View>
+              )}
+            ></FlatList>
+          )}
+        </View>
+
+        <StatusBar style="auto" />
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -117,11 +145,21 @@ const styles = StyleSheet.create({
     margin: 2,
     borderWidth: 1,
     padding: 10,
-    width: 280,
+    flex: 1,
+    marginRight: 10,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   item: {
-    paddingHorizontal: 1,
-    fontSize: 18,
-    height: 44,
+    fontSize: 16,
+    height: 35,
+    flex: 1,
+    marginRight: 10,
+    padding: 5,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
